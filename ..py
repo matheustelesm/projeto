9289f7a6a62bc154a0fbf6268dc7188225388eed
Matheus Teles <161986043+matheustelesm@users.noreply.github.com>
2024-07-26 15:22:49 -0300

    f

--- ..py
+++ ..py
@@ -1,13 +1,31 @@
-import pyautogui as bot
+# import pyautogui as bot
 
-bot.PAUSE = 1.5
+# bot.PAUSE = 1.5
 
-bot.press('win')
-bot.write('google')
-bot.press('enter')
+# bot.press('win')
+# bot.write('google')
+# bot.press('enter')
 
-bot.write('https://chatgpt.com/')
-bot.press('enter')
+# bot.write('https://chatgpt.com/')
+# bot.press('enter')
 
-bot.write('Como criar um codigo em html')
-bot.press('enter')
+# bot.write('Como criar um codigo em html')
+# bot.press('enter')
+
+
+#tabuada do número escolhido
+
+# escolha = int(input("Digite um número: "))
+# print(f"A tabuada do número {escolha} é:")
+
+# for i in range(1, 11):
+#     print(f"{escolha} x {i}: {escolha*i}")
+
+
+#tabuada 1 ao 10.
+
+for num in range(1, 11):
+        print(f"\nTabuada do {num}:")
+        for i in range(1, 11):
+            print(f"{num} x {i} = {num * i}")
+
