c9e05debdbce0354f6a56ebcd0580fcbb3b0421a
Matheus Teles <161986043+matheustelesm@users.noreply.github.com>
2024-09-24 14:57:19 -0300

    itertools

--- aula103.py
+++ aula103.py
@@ -23,7 +23,36 @@
 from itertools import zip_longest
 
 
-lista1 = ['Salvador', 'Ubatuba', 'Belo Horizonte']
-lista2 = ['BA', 'SP', 'MG', 'RJ']
-print(list(zip_longest(lista1, lista2, fillvalue="SEM CIDADE")))
-print(list(zip(lista1, lista2)))
+# lista1 = ['Salvador', 'Ubatuba', 'Belo Horizonte']
+# lista2 = ['BA', 'SP', 'MG', 'RJ']
+# print(list(zip_longest(lista1, lista2, fillvalue="SEM CIDADE")))
+# print(list(zip(lista1, lista2)))
+
+# l1 = [1, 2, 3, 4, 5, 6, 7]
+# l2 = [1, 2, 3, 4]
+# l = []
+# for i in range(len(l2)):
+#     l.append(l1[i] + l2[i])
+
+# print(l)
+
+
+# l1 = [1, 2, 3, 4, 5, 6, 7]
+# l2 = [1, 2, 3, 4]
+# l = []
+# for i, _ in enumerate(l2):
+#     l.append(l1[i] + l2[i])
+
+# print(l)
+# com o zip_longest
+from itertools import zip_longest
+l1 = [1, 2, 3, 4, 5, 6, 7]
+l2 = [1, 2, 3, 4]
+lista_soma = [x + y for x, y in zip_longest(l1 , l2, fillvalue=0)]
+print(lista_soma)
+
+#com o zip
+l1 = [1, 2, 3, 4, 5, 6, 7]
+l2 = [1, 2, 3, 4]
+lista_soma = [x + y for x, y in zip(l1 , l2)]
+print(lista_soma)
