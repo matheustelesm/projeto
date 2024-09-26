4930fb04d49cb115de4be146cd6c1320050342dc
Matheus Teles <161986043+matheustelesm@users.noreply.github.com>
2024-09-26 15:06:35 -0300

    required 106.py

--- ..py
+++ ..py
@@ -24,8 +24,52 @@
 
 #tabuada 1 ao 10.
 
-for num in range(1, 11):
-        print(f"\nTabuada do {num}:")
-        for i in range(1, 11):
-            print(f"{num} x {i} = {num * i}")
+# for num in range(1, 11):
+#         print(f"\nTabuada do {num}:")
+#         for i in range(1, 11):
+#             print(f"{num} x {i} = {num * i}")
 
+
+
+
+# 1. Verificar se um número é par ou ímpar
+# Escreva um programa que peça ao usuário para inserir um número e determine se esse número é par ou ímpar.
+
+# num = int(input("Digite um número: "))
+
+# if num % 2==0:
+#     print("Número par")
+# else:
+#     print("Número ímpar")
+
+
+
+
+# 2. Fatorial de um número
+# Crie um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
+
+num = int(input("Digite um número: "))
+
+fatorial = 1
+for i in range(1, num + 1):
+    fatorial *= i
+print(f"O fatorial de {num} é {fatorial}")
+
+
+# 3. Contagem de vogais em uma string
+# Crie um programa que peça ao usuário para inserir uma string e conte quantas vogais (a, e, i, o, u) ela contém.
+
+palavra = str(input("Digite uma palavra: "))
+
+
+
+
+
+# 4. Ordenar uma lista de números
+# Escreva um programa que peça ao usuário uma lista de números e a ordene em ordem crescente.
+
+# 5. Palíndromo
+# Crie um programa que peça ao usuário uma palavra e verifique se ela é um palíndromo (lida da mesma forma de trás para frente).
+
+# 6. Calculadora Simples
+# Escreva um programa que funcione como uma calculadora básica, capaz de somar, subtrair, multiplicar e dividir dois números.
